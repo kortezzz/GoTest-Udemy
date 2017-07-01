5d04155b34dcdb0957f8cac139ed861e7f26be14
kortezzz <tembomat@gmail.com>
2017-07-01 16:58:25 -0400

    main.go

--- First_test_Go/main.go
+++ First_test_Go/main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	for i := 60; i < 121; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+	}
 }
